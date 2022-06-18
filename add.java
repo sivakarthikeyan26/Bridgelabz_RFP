45950d5565ef3510f46caee15f0ba8ff02d5aa11
Your Name <you@example.com>
2022-06-18 18:12:10 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,19 @@
+package first_code;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=10;
+		int c=0;
+		c=a+b;
+		System.out.println(+c);
+		
+
+	}
+
+}
+
+
+
